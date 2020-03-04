8ac68df46f5191acaa9a86c43c6bfa90dae3171a
dbdeep <36453912+dbdeep@users.noreply.github.com>
2020-03-04 21:36:48 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 function loadSkills(skills){
 		var i=0,j;
-		var skillsInnerHTML='<div>
-		<b>Machine learning<b>: ajdshdjshdsds
-		vjhsdkhsbkdsdskd
-		</div>';
-		
+		var skillsInnerHTML='';
+		while(i<skills.length){
+
+			var row = '<div class="row">';
+			for(j=i;j<i+6&&j<skills.length;j++){
+				var skill = '<div class="col m2"><svg viewBox="0 0 128 128"><path d="'+skills[j].icon+'"></path></svg>'+skills[j].name+'</div>';
+				row+=skill;
+				
+			}
+			row+='</div>';
+			skillsInnerHTML+=row;
+			
+			i=j;
+		}
 		$('#skills').html(skillsInnerHTML);
 }
 
